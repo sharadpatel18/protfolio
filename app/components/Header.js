36e1e8a3377df9b2e5447f3f9236eadad0a12cc1
sharad patel <sharadpatel1807@gmail.com>
2025-01-24 01:42:11 +0530

    some changes

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,19 +1,56 @@
 "use client";
 
+import { motion } from "framer-motion";
 export default function Header() {
-    return (
-      <header className="bg-gray-900 text-white p-4 sticky top-0 z-50 shadow-md">
-        <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-2xl font-bold">Sharad's Portfolio</h1>
-          <nav>
-            <ul className="flex space-x-4">
-              <li><a href="#about" className="hover:text-gray-300">About</a></li>
-              <li><a href="#projects" className="hover:text-gray-300">Projects</a></li>
-              <li><a href="#contact" className="hover:text-gray-300">Contact</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    );
-  }
-  
+  return (
+    <header className="bg-gray-900 text-white p-4 sticky top-0 z-50 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <motion.h1
+          className="text-3xl font-bold mb-6"
+          initial={{ scale: 0.5 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Projects
+        </motion.h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <motion.a
+                href="#about"
+                className="text-3xl font-bold mb-6"
+                initial={{ scale: 0.5 }}
+                animate={{ scale: 1 }}
+                transition={{ duration: 0.5 }}
+              >
+                About
+              </motion.a>
+            </li>
+            <li>
+              <motion.a
+                href="#projects"
+                className="text-3xl font-bold mb-6"
+                initial={{ scale: 0.5 }}
+                animate={{ scale: 1 }}
+                transition={{ duration: 0.5 }}
+              >
+                Projects
+              </motion.a>
+            </li>
+            <li>
+              <motion.a
+                href="#contact"
+                className="text-3xl font-bold mb-6"
+                initial={{ scale: 0.5 }}
+                animate={{ scale: 1 }}
+                transition={{ duration: 0.5 }}
+              >
+                Contact
+              </motion.a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
